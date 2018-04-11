37dc246be3ebd698fab416bf1ff3cd54fea7ea47
736252868@qq.com <736252868@qq.com>
2018-04-11 14:26:35 +0800

    第一次添加

---
+++ src/main/java/com/pingjin/string/SequenceUtil.java
@@ -0,0 +1,28 @@
+package com.pingjin.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.pingjin.common.SnowflakeIdWorker;
+
+public class SequenceUtil {
+
+	private static final SnowflakeIdWorker idWorker = new SnowflakeIdWorker(0, 0);
+
+	/**
+	 * 增加sequenceId值
+	 * 
+	 * @return
+	 */
+	public static long getSeqId() {
+		return idWorker.nextId();
+	}
+
+	public static void main(String[] args) {
+        Map<Long,Object> map = new HashMap<>();
+        for (int i = 0; i < 10000000; i++){
+        	map.put(getSeqId(), "value");
+        }
+        System.out.println(map.size());
+	}
+}
